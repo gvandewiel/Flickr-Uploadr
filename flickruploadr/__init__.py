c0aeb4b32e198e9afe849fc43fe1ecc896691bf4
Gijs van de Wiel <gvandewiel@gmail.com>
2018-12-22 15:25:22 +0100

    cleaned init file

--- flickruploadr/__init__.py
+++ flickruploadr/__init__.py
@@ -2,11 +2,4 @@
 
 This module allows to activate multiple FlickrAPI instances to run concurrently
 """
-
-#from __future__ import print_function
-#from .core import FlickrCore
-#import configparser
-
 __version__ = '2.0.2'
-
-#uploader = FlickrCore()
